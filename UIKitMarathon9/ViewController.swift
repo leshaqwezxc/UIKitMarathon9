73cef1d5742a75349a4a7573f7952409398ffccf
alexeituszowski <atuszovskii@licard.com>
2023-09-25 12:11:04 +0300

    Initial Commit

---
+++ UIKitMarathon9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitMarathon9
+//
+//  Created by alexeituszowski on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
